624e699514adadedf23d7346aaa6a6279c4a3b9b
Alexander Esquivel Barrientos <esq.bar.alex@gmail.com>
2024-09-28 12:12:12 -0600

    Add project files.

---
+++ trabajo-clase-s2/ejercicios.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabajo_clase_s2
+{
+    internal class ejercicios
+    {
+        // Ejercicio 1
+        public void ejercicio1()
+        {
+            Console.Write("Ingrese el primer numero entero: ");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo numero enter: ");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            int suma = numero1 + numero2;
+            int producto = numero1 * numero2;
+
+            Console.WriteLine("La suma de los dos numeros ingresados es:" + suma);
+            Console.WriteLine("La multiplicacion de los dos numeros ingresados es:" + producto);
+
+            Console.ReadKey();
+        }
+        // Ejercicio 2
+        // Ejercicio 3
+        // Ejercicio 4
+        // Ejercicio 5
+        // Ejercicio 6
+        // Ejercicio 7
+        // Ejercicio 8
+        // Ejercicio 9
+        // Ejercicio 10
+        // Ejercicio 11
+        // Ejercicio 12
+    }
+}
